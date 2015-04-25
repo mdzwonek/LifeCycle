3d516416e10ae04d8ff9fc75cc6921cfa6bfbbec
Jaroslaw Wodka <wodka.jaroslaw@gmail.com>
2015-04-25 19:30:55 +0100

    list_bikes added

--- server/LifeCycle/routes/index.js
+++ server/LifeCycle/routes/index.js
@@ -193,4 +193,33 @@ router.post('/add_bike', function(req, res) {
   });
 });
 
+
+router.get('/list_bikes', function(req, res) {
+  var results = [];
+
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+
+    // SQL Query > Select Data
+    var query = client.query("SELECT * FROM public.bike;");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+
+    // Handle Errors
+    if(err) {
+      console.log(err);
+    }
+
+  });
+});
+
 module.exports = router;
