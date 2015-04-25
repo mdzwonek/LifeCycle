32015dab93c1bf1dd237a74f52ed559b2254f287
Mateusz Dzwonek <mateusz.dzwonek@gmail.com>
2015-04-25 13:46:36 +0100

    Initial commit (with iOS project template)

---
+++ iOS/LifeCycle/LifeCycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LifeCycle
+//
+//  Created by Mateusz Dzwonek on 25/04/2015.
+//  Copyright (c) 2015 Mateusz Dzwonek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/LifeCycle/LifeCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LifeCycle
+//
+//  Created by Mateusz Dzwonek on 25/04/2015.
+//  Copyright (c) 2015 Mateusz Dzwonek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
